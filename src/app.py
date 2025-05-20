b55bbc8abbdb6d6c0e353eae187668203538dcc7
Magdaleno <magdaleno.garcia@fraternitas.io>
2025-05-19 22:23:26 -0600

    test case 1

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify({"message": "Hello World"})
+    return jsonify({"message": "Hello World, Test case 1"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
