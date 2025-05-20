458eb0c7b0a128ce02725b81647c08630cb9d0d4
AaronAscencioM <rolzdeath@gmail.com>
2025-05-19 21:16:35 -0600

    App returning json response

--- src/app.py
+++ src/app.py
@@ -1,10 +1,10 @@
-# src/app.py
-from flask import Flask
+from flask import Flask, jsonify
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hola desde un microservicio Flask en AWS ECS"
+    return jsonify({"message": "Hello World"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
